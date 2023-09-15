35937d91ff5c63c51f11e4ffd49a4f36043b42dc
Muhammad Hermas Yuda Pamungkas <hermas.yuda@gmail.com>
2023-09-15 20:07:45 +0700

    Refactor page to navigation jetpack

--- app/src/main/java/com/catnip/activityfragmentexample/presentation/fragmentthree/FragmentThree.kt
+++ app/src/main/java/com/catnip/activityfragmentexample/presentation/fragmentthree/FragmentThree.kt
@@ -1,5 +1,6 @@
 package com.catnip.activityfragmentexample.presentation.fragmentthree
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,20 +10,31 @@ import com.catnip.activityfragmentexample.R
 import com.catnip.activityfragmentexample.databinding.FragmentOneBinding
 import com.catnip.activityfragmentexample.databinding.FragmentThreeBinding
 import com.catnip.activityfragmentexample.databinding.FragmentTwoBinding
+import com.catnip.activityfragmentexample.presentation.otheractivity.OtherActivity
 
 class FragmentThree : Fragment() {
-    private lateinit var binding : FragmentThreeBinding
+    private lateinit var binding: FragmentThreeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentThreeBinding.inflate(inflater,container,false)
+        binding = FragmentThreeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setClickListener()
+    }
 
+    private fun setClickListener() {
+        binding.btnNavigateOther.setOnClickListener {
+            navigateToOtherActivity()
+        }
+    }
+
+    private fun navigateToOtherActivity() {
+        OtherActivity.startActivity(requireContext(),"Hermas",27)
     }
 }
